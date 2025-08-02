1b9cd3b3df05d870b36a542a6b1c1d054c1da3fe
VishnuAM01 <vishnu.arakkal.13@gmail.com>
2025-08-02 19:18:16 +0530

    Create StringReversingWithoutArray.java
    String reversing without array and less number of code

---
+++ StringReversingWithoutArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class StringReversingWithoutArray {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        //Accepting the input string from user
+        System.out.println("Enter the string");
+        String str=sc.next();
+        int len=str.length();
+        String str1="";
+        
+        //Storing the string into an array
+        for(int i=len-1;i>=0;i--){
+            str1+=str.charAt(i);
+        }
+        
+        //Printing the reversed string
+        System.out.println(str1);
+        sc.close();
+    }
+}
