c55c78b504765b4f9f521287dbe25e3da978d86a
VishnuAM01 <vishnu.arakkal.13@gmail.com>
2025-09-20 15:17:40 +0530

    Create HashmapPractice.java

---
+++ HashmapPractice.java
@@ -0,0 +1,16 @@
+package Logic_Problems_Solutions;
+import java.util.HashMap;
+
+public class HashmapPractice {
+
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("A","Vishnu");
+		map.put("B", "Diya");
+		map.put("C","Rahul");
+		
+		System.out.println(map.get("A"));
+
+	}
+
+}
